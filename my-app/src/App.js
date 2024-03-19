daffc734979513794ed41f646baba9822381b9ab
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-03-19 12:46:39 -0400

    New

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const name = "sai rangineeni";
+  const age = 19;
+
+  return (
+    <h1>
+      Hello {name} you are {age}
+    </h1>
+  );
+}
+
+export default App;
