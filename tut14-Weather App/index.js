bdfd6c38aadd87bdc71b4d21668938f35ec8ab8d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-02 14:06:21 -0400

    Weather App PT2 - API

--- tut14-Weather App/index.js
+++ tut14-Weather App/index.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
-const Latitude = document.getElementById("latitude");
-const Longitude = document.getElementById("longitude");
+const LatitudeInput = document.getElementById("latitude");
+const LongitudeInput = document.getElementById("longitude");
 const result = document.getElementById("result");
 const aqiResult = document.getElementById("AQI");
 const COResult = document.getElementById("CO");
@@ -9,3 +9,32 @@ const O3Result = document.getElementById("O3");
 const PM2Result = document.getElementById("PM2");
 const PM10Result = document.getElementById("PM10");
 const SO2Result = document.getElementById("SO2");
+
+form.addEventListener("submit", (event) => {
+  //to prevent default settings or actions
+  event.preventDefault();
+
+  //store values here using .vaule
+  const latitude = LatitudeInput.value;
+  const longitude = LongitudeInput.value;
+
+  //api request and insert ${}
+  const url = `https://air-quality.p.rapidapi.com/history/airquality?lon=${longitude}&lat=${latitude}`;
+
+  //api key
+  const options = {
+    //get method
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c0113fc877msh6d26dc60fe7afa6p18dc03jsnf933b9f588c9",
+      "X-RapidAPI-Host": "air-quality.p.rapidapi.com",
+    },
+  };
+
+  //request, pass these
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
