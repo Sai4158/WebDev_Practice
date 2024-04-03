324246143559ee9fa79f8f452372ef0c26b212e5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-02 22:11:27 -0400

    Final Air Pollution App

--- tut14-Weather App/index.js
+++ tut14-Weather App/index.js
@@ -1,7 +1,8 @@
+//Get them from
 const form = document.getElementById("form");
 const LatitudeInput = document.getElementById("latitude");
 const LongitudeInput = document.getElementById("longitude");
-const result = document.getElementById("result");
+const result1 = document.getElementById("result");
 const aqiResult = document.getElementById("AQI");
 const COResult = document.getElementById("CO");
 const NO2Result = document.getElementById("NO2");
@@ -10,31 +11,35 @@ const PM2Result = document.getElementById("PM2");
 const PM10Result = document.getElementById("PM10");
 const SO2Result = document.getElementById("SO2");
 
+//when submit is clicked this will happen
 form.addEventListener("submit", (event) => {
-  //to prevent default settings or actions
+  //to prevent default settings
   event.preventDefault();
 
   //store values here using .vaule
   const latitude = LatitudeInput.value;
   const longitude = LongitudeInput.value;
 
-  //api request and insert ${}
-  const url = `https://air-quality.p.rapidapi.com/history/airquality?lon=${longitude}&lat=${latitude}`;
+  //API url and key and gets lat, lon by ${}
+  const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=b53cff135a56178c33f88448a32074b5`;
 
-  //api key
-  const options = {
-    //get method
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "c0113fc877msh6d26dc60fe7afa6p18dc03jsnf933b9f588c9",
-      "X-RapidAPI-Host": "air-quality.p.rapidapi.com",
-    },
-  };
-
-  //request, pass these
-  fetch(url, options)
+  //Will fetch url and get the data
+  fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    .then((result) => {
+      console.log(result);
+
+      //will print each data based on the documentation
+      const airQualityData = result.list[0];
+      aqiResult.textContent = `${airQualityData.main.aqi}`;
+      COResult.textContent = `${airQualityData.components.co}`;
+      NO2Result.textContent = `${airQualityData.components.no2}`;
+      O3Result.textContent = `${airQualityData.components.o3}`;
+      PM2Result.textContent = `${airQualityData.components.pm2_5}`;
+      PM10Result.textContent = `${airQualityData.components.pm10}`;
+      SO2Result.textContent = `${airQualityData.components.so2}`;
+
+      // Display the results when clicked
+      result1.style.display = "flex";
     });
 });
