0f31576984a6473d94948a3c8a10b4479c1dde53
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-06 16:22:19 -0400

    Intro to NodeJS

---
+++ tut15-Intro-to-nodejs/index.js
@@ -0,0 +1,15 @@
+let a = "hello I am Sai";
+console.log(a);
+
+const greeting = 'Hello, Node.js!';
+console.log(greeting);
+
+
+
+//this is how you run using the nodejs in the terminal 
+
+// //PS C:\Users\saira\OneDrive - Vs_Code> cd tut15-Intro-to-nodejs
+// PS C:\Users\saira\OneDrive - Vs_Code\tut15-Intro-to-nodejs> node index.js
+// hello I am Sai
+// Hello, Node.js!
+
