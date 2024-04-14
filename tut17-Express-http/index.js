504cd5de71f8c95f9be00b1fcb04c91772f03627
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-14 14:46:42 -0400

    Handling GET Requests:/ pages

---
+++ tut17-Express-http/index.js
@@ -0,0 +1,18 @@
+//get request in express
+const express = require("express");
+const app = express();
+
+//this is page1
+app.get("/", (req, res) => {
+  res.send("Hello this is page one");
+});
+
+//this is page2 
+app.get("/about", (req, res) => {
+  res.send("this is page two");
+});
+
+const port = 3000;
+app.listen(3000, () => {
+  console.log(`port is running at ${port}`);
+});
