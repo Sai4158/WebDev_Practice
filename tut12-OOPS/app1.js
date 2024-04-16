c7af9f7557281da1f344845635947e93844e90df
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-16 02:51:07 -0400

    Spread syntax (...)

---
+++ tut12-OOPS/app1.js
@@ -0,0 +1,12 @@
+const number = [233, 2434, 5768, 2344, 4636, 142354, 73421, 5674, 2321, 32321];
+
+//this will print the smallest number from the array number
+let smallest = Math.min(...number);
+console.log(`Smallest number ${smallest}`);
+
+//this will print the max
+let largest = Math.max(...number);
+console.log(`Largest number ${largest}`);
+
+//Smallest number 233
+//Largest number 142354
