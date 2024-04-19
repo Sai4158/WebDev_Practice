c7d33f872370cd2c5209c79dead52110505726d6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-18 23:58:57 -0400

    The fragment syntax in react

--- tut18-React-1/react-1/app-test/src/App.js
+++ tut18-React-1/react-1/app-test/src/App.js
@@ -1,19 +1,35 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
 
 function App() {
-  const one = 1;
   return (
-    <div style={{ justifyContent: "center", display: "flex" }}>
-      <h1 style={{ color: "blue" }}>This is a bule car</h1>
-      <br />
-      <img
-        src={
-          "https://imageio.forbes.com/specials-images/imageserve/6064b148afc9b47d022718d1/Hennessey-Venom-F5/960x0.jpg?height=473&width=711&fit=bounds"
-        }
-      />
-    </div>
+    <>
+      <div
+        style={{
+          justifyContent: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ color: "blue" }}>This is a blue car</h1>
+        <img
+          src="https://imageio.forbes.com/specials-images/imageserve/6064b148afc9b47d022718d1/Hennessey-Venom-F5/960x0.jpg?height=473&width=711&fit=bounds"
+          alt=" Venom F5"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+      </div>
+
+      <div>
+        <ul>
+          <li>car 1</li>
+          <li>car 2</li>
+          <li>car 3</li>
+          <li>car 4</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
