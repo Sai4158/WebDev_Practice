10977ff135649a58001cf1332438b0c5b9e09e36
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-22 21:04:53 -0400

    React-if/else

---
+++ tut19-react/if-else/my-app/src/App.js
@@ -0,0 +1,11 @@
+import ElementVariablesExample from "./Components/condition";
+
+function App() {
+  return (
+    <>
+      <ElementVariablesExample />
+    </>
+  );
+}
+
+export default App;
---
+++ tut19-react/if-else/my-app/src/Components/condition.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class ElementVariablesExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  //if else
+  //if this happens this will happen
+  render() {
+    let content;
+    if (this.state.isLoggedIn) {
+      content = <p>Welcome, User!</p>;
+    } else {
+      content = <p>Please log in</p>;
+    }
+
+    return <div>{content}</div>;
+  }
+}
+
+export default ElementVariablesExample;
