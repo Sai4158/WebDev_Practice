129dd69885bf79751f5980534193549117aae479
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-26 01:44:52 -0400

    CSS in React

--- tut18-react1/react1/my-app/src/App.js
+++ tut18-react1/react1/my-app/src/App.js
@@ -1,5 +1,7 @@
 import Apple from "./apple";
 import Sample from "./Nested Components/Sample";
+import "./App.css";
+import "./sample.css";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       {/* Use the component with a capital letter */}
       <Apple />
       <Sample />
+
+      <h1>this is sample css file </h1>
     </div>
   );
 }
