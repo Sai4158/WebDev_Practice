5d9e127c5920e91fffe2d866fd703e8349d3d076
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-29 01:17:06 -0400

    Styling and img in react

--- tut18-react1/react1/my-app/src/App.js
+++ tut18-react1/react1/my-app/src/App.js
@@ -14,6 +14,18 @@ function App() {
       <h1>
         I like {a}, {b}
       </h1>
+
+      <div className="container">
+        <p>this is paragraph</p>
+        <img className="img2"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsEFTJyiprf8CPYc_WaV6T97zrPVleDAMtnbkdb9kEA&s"
+          alt=""
+        />
+        <img className="img1"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsEFTJyiprf8CPYc_WaV6T97zrPVleDAMtnbkdb9kEA&s"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
