f6097cbb8402bf5af59eee2ba8af2ee8dae98053
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-01 19:46:54 -0400

    Added tailwindcss to it

--- tut18-react2-props/PROPS-1/my-app1/src/Student.jsx
+++ tut18-react2-props/PROPS-1/my-app1/src/Student.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Student = (props) => {
   return (
-    <div>
-      <h1>Hello I am {props.name4}, I like playing around with react</h1>
+    <div className="bg-gray-500">
+      <h1 className="w-20">
+        Hello I am span <strong>{props.name4}</strong>, I like playing around
+        with react
+      </h1>
     </div>
   );
 };
