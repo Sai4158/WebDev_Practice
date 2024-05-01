0b06df5d6a7b3cfdd4221279e1add46082d6268a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-01 19:40:10 -0400

    send data using props

---
+++ tut18-react2-props/PROPS-1/my-app1/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <Student name4="Sai" />
+    </div>
+  );
+}
+
+export default App;
---
+++ tut18-react2-props/PROPS-1/my-app1/src/Student.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Student = (props) => {
+  return (
+    <div>
+      <h1>Hello I am {props.name4}, I like playing around with react</h1>
+    </div>
+  );
+};
+
+export default Student;
