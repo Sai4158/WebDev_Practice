8d7517b76d0cd6cc437b63cf6eb64141c9710a00
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-02 18:42:42 -0400

    use effect hook in react

--- tut18-react2-props/Conditional_Props/my-app2/src/Use_State/Use.jsx
+++ tut18-react2-props/Conditional_Props/my-app2/src/Use_State/Use.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Use = (props) => {
-  const [city, setCity] = useState("Hyderbad");
+  const [city, setCity] = useState("hyd");
+
+  //this is use effect
+  useEffect(() => {
+    if (city === "hyd") {
+      setCity("banglore");
+    } else {
+      setCity("Philly");
+    }
+  });
 
   //conditional statments
-  if (city === "Hyderbad") {
-    setCity("banglore");
-  }
-
   return <div>{city}</div>;
 };
 
