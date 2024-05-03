c244fe85ccdbaaf0b078538e69fa062410fff607
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-03 14:22:48 -0400

    use effect - react

--- tut18-react2-props/Conditional_Props/my-app2/src/Use_State/Use.jsx
+++ tut18-react2-props/Conditional_Props/my-app2/src/Use_State/Use.jsx
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from "react";
 
 const Use = (props) => {
   const [city, setCity] = useState("hyd");
+  const [car, setCar] = useState("audi");
 
-  //this is use effect
+  //this is use effect -1
   useEffect(() => {
     if (city === "hyd") {
       setCity("banglore");
     } else {
       setCity("Philly");
+    }
+  });
+
+  // use effect -2
+  useEffect(() => {
+    if (car === "audi") {
+      setCar("bwm");
+    } else {
+      setCar("benz");
     }
   });
 
