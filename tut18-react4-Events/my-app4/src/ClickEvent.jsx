9963c7240dedc56a1bb2a0f2e02770542af29875
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-06 09:51:31 -0400

    Events in react - number counter

--- tut18-react4-Events/my-app4/src/ClickEvent.jsx
+++ tut18-react4-Events/my-app4/src/ClickEvent.jsx
@@ -4,12 +4,29 @@ import { useState } from "react";
 const ClickEvent = () => {
   const [number, setNumber] = useState(0);
 
+  //this will increase the number
+  const Increment = () => {
+    setNumber(number + 1);
+  };
+
+  //this will decrease the number
+  const Decreament = () => {
+    if (number > 0) {
+      setNumber(number - 1);
+    }
+  };
+
+  const reset = () => {
+    setNumber((Number = 0));
+  };
+
   return (
     <div>
-      {number}
+      <h1>{number}</h1>
       <div>
-        <button>Increment</button>
-        <button>Decreament</button>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Decreament}>Decreament</button>
+        <button onClick={reset}>Reset</button>
       </div>
     </div>
   );
