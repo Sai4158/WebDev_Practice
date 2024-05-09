448a76535961586aef4c40a1e32ab2b14b21c091
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-09 17:39:32 -0400

    Shows on screen when you input - react

--- tut18-react6-FormEvents/myapp6/src/Component/FormExample.jsx
+++ tut18-react6-FormEvents/myapp6/src/Component/FormExample.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 const FormExample = () => {
+  const [username, setUsername] = useState("");
+
+  //will update it in username using the useState
+  const getUserName = (event) => {
+    setUsername(event.target.value);
+  };
+
   return (
     <div className="formSection">
+      <h2>{username}</h2>
       <div>
-        <input className="inputdiv" type="text" placeholder="input ur text" />
+        <input
+          className="inputdiv"
+          type="text"
+          placeholder="input ur text"
+          onChange={getUserName}
+        />
         <br />
-        <button className="submit" type="submit">
+        <button className="submit" type="submit" onClick={}>
           Submit
         </button>
       </div>
