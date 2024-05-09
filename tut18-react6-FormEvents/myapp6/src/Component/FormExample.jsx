37dc52e129aec24ca82e120622e522c479e0be7e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-09 18:01:39 -0400

    Will only update the text on click in react using useState

--- tut18-react6-FormEvents/myapp6/src/Component/FormExample.jsx
+++ tut18-react6-FormEvents/myapp6/src/Component/FormExample.jsx
@@ -2,27 +2,38 @@ import React from "react";
 import { useState } from "react";
 
 const FormExample = () => {
+  // this is store the data -1
   const [username, setUsername] = useState("");
+  //this is to update only when clicked -2
+  const [userdetails, setUserdeatails] = useState("");
 
-  //will update it in username using the useState
+  //will update it in username using the useState -1
   const getUserName = (event) => {
     setUsername(event.target.value);
   };
 
+  //will only update when submit button is clicked -2
+  const update = (event) => {
+    event.preventDefault();
+    setUserdeatails(username);
+  };
+
   return (
     <div className="formSection">
-      <h2>{username}</h2>
+      <h2>Hello, {userdetails}</h2>
       <div>
-        <input
-          className="inputdiv"
-          type="text"
-          placeholder="input ur text"
-          onChange={getUserName}
-        />
-        <br />
-        <button className="submit" type="submit" onClick={}>
-          Submit
-        </button>
+        <form onClick={update}>
+          <input
+            className="inputdiv"
+            type="text"
+            placeholder="input ur text"
+            onChange={getUserName}
+          />
+          <br />
+          <button className="submit" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
