a0af5d79614de262c05ba2915f7f3648a7be9a44
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-09 10:58:12 -0400

    React Form Events

---
+++ tut18-react6-FormEvents/myapp6/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormExample from "./Component/FormExample";
+
+function App() {
+  return (
+    <div>
+      <FormExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ tut18-react6-FormEvents/myapp6/src/Component/FormExample.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FormExample = () => {
+  return (
+    <div className="formSection">
+      <div>
+        <input className="inputdiv" type="text" placeholder="input ur text" />
+        <br />
+        <button className="submit" type="submit">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FormExample;
