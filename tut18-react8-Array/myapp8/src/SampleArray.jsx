70bbb9ed7e38c609d71846b33fe71f50be82b44e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-13 00:32:30 -0400

    get values from Object in Array using Map method

--- tut18-react8-Array/myapp8/src/SampleArray.jsx
+++ tut18-react8-Array/myapp8/src/SampleArray.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 
-let sample = ["apple", "mango", 35, { username: "Sai" }];
+let fruits = ["apple", "mango", 35, { username: "Sai" }];
 
+let players = [
+  {
+    cricket: "dhoni",
+    tennis: "sania",
+    chess: "anand",
+    hockey: "Dyan Chand",
+  },
+];
 const SampleArray = () => {
   //print the array using the {} and the index you want to print
-  return <div>{sample[1]}</div>;
+  return (
+    <div>
+      {fruits[1]}
+      <br />
+
+      {/*print by using index*/}
+      {players[0].cricket}
+
+      {players.map((item) => {
+        return <div>{item.hockey}</div>;
+      })}
+
+      {players.map((item1) => {
+        return <div>{item1.chess}</div>;
+      })}
+    </div>
+  );
 };
 
 export default SampleArray;
