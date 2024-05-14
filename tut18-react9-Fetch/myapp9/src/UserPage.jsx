6c458c0bdde48f0d9e93590e068aca3781ecad7a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-14 17:56:05 -0400

    Using fetch() method, to get api info and print it using map()

---
+++ tut18-react9-Fetch/myapp9/src/UserPage.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+//get the api lin
+const userDetails = "https://jsonplaceholder.typicode.com/posts";
+
+const UserPage = () => {
+  //use state to store data
+  const [user, setUser] = useState([]);
+
+  //use asnyc to fetch data and then store it into user
+  const userHandle = async () => {
+    const response = await fetch(userDetails);
+    const newData = await response.json();
+    setUser(newData);
+  };
+
+  //this use effect will make sure it will only run once
+  //use effect will stop reapted printing
+  useEffect(() => {
+    console.log(userHandle());
+  }, []);
+
+  //the display using the map function
+  return (
+    <div>
+      {user.map((item) => {
+        return (
+          <div className="userSection" style={{ color: "red" }}>
+            <h1>{item.title}</h1>
+
+            <div style={{ color: "blue" }}>
+              <h2>{item.body}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default UserPage;
