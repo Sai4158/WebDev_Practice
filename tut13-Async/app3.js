37110829003791f30497bb783e611291109254cd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-14 17:18:45 -0400

    call back function (set timeout)

---
+++ tut13-Async/app3.js
@@ -0,0 +1,12 @@
+console.log("1");
+console.log("2");
+
+//here is how to use set timeout
+//this will print after 3 secs
+//this is call back function
+setTimeout(() => {
+  console.log("3");
+}, 3000);
+
+console.log("4");
+console.log("5");
