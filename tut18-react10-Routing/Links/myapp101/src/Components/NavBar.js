e394fc5c43a5785409692597985fe7d1fdb77fc4
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-20 02:39:11 -0400

    Active links in react

--- tut18-react10-Routing/Links/myapp101/src/Components/NavBar.js
+++ tut18-react10-Routing/Links/myapp101/src/Components/NavBar.js
@@ -1,13 +1,21 @@
 import React from "react";
 //this is for linkning pages.
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const Navlinkstyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
   return (
+    // or u
     <nav>
       {/* use capital Link so it will use from react router dom */}
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/ANY">404 Page</NavLink>
     </nav>
   );
 };
