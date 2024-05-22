f47713acd0c9f433c5d3be88f88ff0358c74a057
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-22 17:21:03 -0400

    Routing in react vite

--- tut18-react14-Vite/vite-project/src/App.jsx
+++ tut18-react14-Vite/vite-project/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import First from "./Routing/First";
+import SecondPage from "./Routing/SecondPage";
+import Third from "./Routing/Third";
+import Fourth from "./Routing/Fourth";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <First />
+      <SecondPage />
+      <Third />
+      <Fourth />
+    </div>
+  );
 };
 
 export default App;
