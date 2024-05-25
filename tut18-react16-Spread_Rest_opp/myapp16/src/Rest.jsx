9b3aaddfa04703d8e296f1a2d864943750a9e9e7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-24 21:25:08 -0400

    Adding each num in array using reduce method

--- tut18-react16-Spread_Rest_opp/myapp16/src/Rest.jsx
+++ tut18-react16-Spread_Rest_opp/myapp16/src/Rest.jsx
@@ -7,6 +7,12 @@ function sum(...number) {
   return number.reduce((acc, num) => acc + num, 0);
 }
 
+// this wil display in the console
+console.log(sum(1, 2, 3));
+console.log(sum(1, 2, 3, 5, 6, 7));
+console.log(sum(43, 45, 56, 2345, 645));
+console.log(sum(234, 4353324, 435, 23, 43));
+console.log(sum(768, 456, 768234, 54, 6, 7, 35));
 const Rest = () => {
   return (
     <div>
