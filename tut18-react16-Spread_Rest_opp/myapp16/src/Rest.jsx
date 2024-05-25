88d34abad441412c37841dead881c97c8f39685f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-24 21:41:53 -0400

    this is a simple a program using the rest operator

--- tut18-react16-Spread_Rest_opp/myapp16/src/Rest.jsx
+++ tut18-react16-Spread_Rest_opp/myapp16/src/Rest.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-/* this is a simple a program using the rest ooperator  */
+/* this is a simple a program using the rest operator  */
 /* this will add all the aray number and give the output */
 
 function sum(...number) {
-  return number.reduce((acc, num) => acc + num, 0);
+  return number.reduce((num1, num2) => num1 + num2, 0);
 }
 
 // this wil display in the console
@@ -13,6 +13,16 @@ console.log(sum(1, 2, 3, 5, 6, 7));
 console.log(sum(43, 45, 56, 2345, 645));
 console.log(sum(234, 4353324, 435, 23, 43));
 console.log(sum(768, 456, 768234, 54, 6, 7, 35));
+
+console.log("-----------------------------------");
+
+//function 2
+function add(...nums) {
+  return nums.reduce((num1, num2) => num1 + num2, 0);
+}
+console.log("add: " + add(11, 11, 11, 11, 11));
+
+//return
 const Rest = () => {
   return (
     <div>
