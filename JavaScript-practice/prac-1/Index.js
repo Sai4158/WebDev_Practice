bbba6f329021ac90ea8ebe1b739eea48566e6571
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-26 16:10:28 -0400

    Js practice  -  arrays, datatypes and operators

---
+++ JavaScript-practice/prac-1/Index.js
@@ -0,0 +1,22 @@
+var bunty = "raj";
+
+var bunty = "hello";
+
+console.log(bunty);
+
+// -----------------------------
+var a = "this is text meessage";
+
+test = 10 > 20;
+
+console.log(test);
+// ---------------------------
+//will display on the screen
+//<br> use for new line
+document.write("This is javascript text messages<br>");
+document.write(1223);
+
+// -----------------------------------------------
+
+let fruits = ["apple", "mango", 55, { name: "sai" }];
+console.table(fruits);
