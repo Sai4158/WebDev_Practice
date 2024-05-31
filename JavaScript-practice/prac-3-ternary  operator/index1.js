5d219a3ac564eedab97998a07b78d14a534c0f9d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-31 00:35:15 -0400

    JS - ternary and conditional operator

---
+++ JavaScript-practice/prac-3-ternary  operator/index1.js
@@ -0,0 +1,13 @@
+//works
+const ab = 2 + 5 == 5 ? "Yess" : "no!";
+document.write(ab);
+
+//br in javascript
+document.write("color is red <br>");
+
+//2nd
+var x = 100;
+var y = 50;
+
+x < y ? document.write("color is green") : document.write("color is red");
+//red
