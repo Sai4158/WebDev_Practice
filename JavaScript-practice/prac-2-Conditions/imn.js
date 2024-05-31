ece750b43bd0c4d1696dc04963d19a361fea43aa
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-31 00:34:16 -0400

    JS - conditions

---
+++ JavaScript-practice/prac-2-Conditions/imn.js
@@ -0,0 +1,14 @@
+var game = "cricket";
+
+let captian = "dohni";
+
+const tennis = "sania";
+
+//using a and statment to print
+if (game === "cricket" && captian === "dohni") {
+  document.write("this game is ", game);
+} else if (tennis === "sania") {
+  document.write("this is tennis");
+} else {
+  document.write("this is not a game");
+}
