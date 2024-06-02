58143d700b6d04f96e38face8e4556445325fa1e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-02 14:27:17 -0400

    Push and lenght method in js

---
+++ JavaScript-practice/prac-7-Methods/app.js
@@ -0,0 +1,11 @@
+//methods in js
+
+let sports = ["cricket", "baseball"];
+
+//push will show in the last
+sports.push("tennis");
+
+document.write(sports + "<br>");
+
+//this will show the lenght of the array
+document.write(sports.length);
