dd53eb42b42c494aaa2ec35e93608b49c143759c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-07 19:27:09 -0400

    Conditional IndexOf

--- JavaScript-practice/prac-8-indexOf/app.js
+++ JavaScript-practice/prac-8-indexOf/app.js
@@ -5,6 +5,7 @@ document.write("<br>");
 
 document.write(student);
 document.write("<br>");
+
 document.write(student.indexOf("anul "));
 document.write(student.indexOf("Fewf "));
 document.write(student.indexOf("3refs "));
@@ -12,9 +13,14 @@ document.write(student.indexOf("3refs "));
 //this is how you push index if it is -1
 document.write("<br>");
 var newstudents = student.indexOf("satish");
-
 if (newstudents === -1) {
   student.push("satish");
 }
 document.write(student);
 //anul ,3refs ,Fewf ,ewfwf ,satish
+
+var studen = student.indexOf("hsi");
+if (studen === -1) {
+  student.push("hsi");
+}
+document.write(student);
