f08f3f938e5cdab85dfac70cc6143b2f7aa1109a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-07 20:07:19 -0400

    extra comments

--- tut20-NextJs4-Catch-all-Segments/myapp/src/app/docs/[...slug]/page.tsx
+++ tut20-NextJs4-Catch-all-Segments/myapp/src/app/docs/[...slug]/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const docs = ({ params }) => {
+  // if else statements
   if (params.slug.length) {
     return (
       <h1>
+        {/* [0] or [1] is url /after docs */}
         views for docs {params.slug[0]} and {params.slug[1]} and{" "}
         {params.slug[2]} AND {params.slug[3]}
       </h1>
