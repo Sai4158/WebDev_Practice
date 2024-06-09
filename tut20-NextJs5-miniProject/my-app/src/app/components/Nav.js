226162aefa8dcf192080fad38d6e392577d612bf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-09 14:25:29 -0400

    Importing components in next js

---
+++ tut20-NextJs5-miniProject/my-app/src/app/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="center">
+        <ul>get to know me</ul>
+        <ul>contacnt me</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
