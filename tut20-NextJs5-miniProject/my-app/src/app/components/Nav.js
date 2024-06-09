401b047d60cb13ee7972c86dd4807bf50b28c555
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-09 16:40:32 -0400

    Added some components

--- tut20-NextJs5-miniProject/my-app/src/app/components/Nav.js
+++ tut20-NextJs5-miniProject/my-app/src/app/components/Nav.js
@@ -3,9 +3,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="center">
+      <div className="center flex-row">
         <ul>get to know me</ul>
         <ul>contacnt me</ul>
+        <ul>hi</ul>
       </div>
     </div>
   );
