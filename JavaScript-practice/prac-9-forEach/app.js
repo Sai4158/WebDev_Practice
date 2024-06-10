5a0ee117c7c855f68fc237f3cf0a1a3186aefd54
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-10 01:02:52 -0400

    Using foreach, callback functions and concat method

--- JavaScript-practice/prac-9-forEach/app.js
+++ JavaScript-practice/prac-9-forEach/app.js
@@ -26,3 +26,21 @@ score.forEach(function (apple) {
 // 10
 // 50
 // 350
+
+// ---------------------
+document.write("<br>");
+document.write("<br>");
+
+let mycat = [1, 2, 3, 4, 5, 56, 6, 6, 7, 78];
+let mydog = [4.3, 6, 45, 6, 45, 566.6];
+
+let both = mycat.concat(mydog);
+
+document.write(both);
+
+//call back funtion
+document.write("<br>");
+
+both.forEach(function (item) {
+  document.write(item - 5);
+});
