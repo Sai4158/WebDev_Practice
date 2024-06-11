cab30288f218f0b9c3a1c0ff4172e9c553228c44
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-11 14:00:12 -0400

    For loop practice in js

---
+++ JavaScript-practice/prac-11-Loops/app.js
@@ -0,0 +1,15 @@
+// this is how to for loop in js
+
+for (i = 1; i < 10; i++) {
+  document.write(i + ". hello I am sai" + "<br>");
+}
+
+// // 1. hello I am sai
+// 2. hello I am sai
+// 3. hello I am sai
+// 4. hello I am sai
+// 5. hello I am sai
+// 6. hello I am sai
+// 7. hello I am sai
+// 8. hello I am sai
+// 9. hello I am sai
