a69d753e5ea91022efcc1f4b583fa14a743ad89f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-11 11:03:11 -0400

    Split method in js

---
+++ JavaScript-practice/prac-10-StringMethods/app.js
@@ -0,0 +1,8 @@
+//string methods
+// split()
+
+var sample = "Hello i am sai and I go to psu";
+
+//so basically whatever you put in the commas it will be gone/deleted
+document.write(sample.split(""));
+// H,e,l,l,o, ,i, ,a,m, ,s,a,i, ,a,n,d, ,I, ,g,o, ,t,o, ,p,s,u
