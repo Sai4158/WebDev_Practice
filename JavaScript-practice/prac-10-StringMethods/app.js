8cb3320ceb9a261607bbcbd933d3c07bc06a9795
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-11 11:39:07 -0400

    Trim in js

--- JavaScript-practice/prac-10-StringMethods/app.js
+++ JavaScript-practice/prac-10-StringMethods/app.js
@@ -6,3 +6,10 @@ var sample = "Hello i am sai and I go to psu";
 //so basically whatever you put in the commas it will be gone/deleted
 document.write(sample.split(""));
 // H,e,l,l,o, ,i, ,a,m, ,s,a,i, ,a,n,d, ,I, ,g,o, ,t,o, ,p,s,u
+
+document.write("<br>");
+
+//tirm will remove the empty spaces.
+student = "  sai   ";
+console.log(student);
+document.write(student.trim());
