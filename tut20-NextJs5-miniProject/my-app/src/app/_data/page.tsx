336c30f0812c338274d3896a5a83fee25ee47179
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-13 02:50:44 -0400

    Private folders in next js

---
+++ tut20-NextJs5-miniProject/my-app/src/app/_data/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const page = () => {
+  return <div>you can not see this in the web page</div>;
+};
+
+export default page;
+
+//this is a private floder, you cannot access it if you put underscore before you start the folder name
