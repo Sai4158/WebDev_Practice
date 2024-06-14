ee752ffdf5a8315cad9fa78c2f1e453a9b155c48
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-14 02:12:43 -0400

    Loops Practical

--- JavaScript-practice/prac-12-Do-while-Loop/prac-11-Loops/app.js
+++ JavaScript-practice/prac-12-Do-while-Loop/prac-11-Loops/app.js
@@ -8,3 +8,16 @@ do {
 } while (y < 20);
 
 //I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai
+
+// this is a practice for the loops I have learned
+
+var amount = 100;
+var days = 60;
+var interest = 10;
+
+for (i = 1; i <= days; i++) {
+  if (i % 4 === 0) {
+    amount += interest;
+  }
+  document.write("your amount is " + amount + " for " + i + " day " + "<br>");
+}
