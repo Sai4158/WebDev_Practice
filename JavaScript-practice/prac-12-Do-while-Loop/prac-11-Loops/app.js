737253b2d2d5d0a821df81f1e0dcd2a33152bdaa
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-14 01:43:06 -0400

    Do while loop in js

---
+++ JavaScript-practice/prac-12-Do-while-Loop/prac-11-Loops/app.js
@@ -0,0 +1,10 @@
+//this is do while loop
+
+let y = 0;
+
+do {
+  document.write("I am sai ");
+  y++;
+} while (y < 20);
+
+//I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai I am sai
