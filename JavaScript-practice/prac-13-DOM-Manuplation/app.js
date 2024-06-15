0474a8683ccd858f8c5f10cbca35827cc4114889
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-15 02:30:14 -0400

    text Content method

--- JavaScript-practice/prac-13-DOM-Manuplation/app.js
+++ JavaScript-practice/prac-13-DOM-Manuplation/app.js
@@ -5,8 +5,12 @@ const a = document.getElementById("company");
 //innner text will modif the text in the html document
 a.innerText = "I am Sai";
 
+// or you can also you the text Content method too
+a.textContent = "I am not sai";
 //----------------
 
 const b = document.getElementById("company1");
 //inner html will add it h1 tags in the html code
 b.innerHTML = "<h1>Hello I am sai too</h2>";
+
+// text conctext in js
