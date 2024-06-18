fc2211edb44e22056b4aad2ee2e6e5e609aace2a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-19 03:58:00 +0530

    iterates each number using forEach

---
+++ JavaScript-practice/prac-1/recap/app.js
@@ -0,0 +1,9 @@
+//for each -  Will gover each number and it takes a funtion
+let num = [, 4, 5, 6, 45, 6];
+
+num.forEach((number) => {
+  document.write(number * 2 + " ");
+});
+//8 10 12 90 12
+
+document.write("<br>");
