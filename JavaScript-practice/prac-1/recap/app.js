8f35c5b641c4f97104b3d349bab229e1d7924b45
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-19 04:16:04 +0530

    map function in js

--- JavaScript-practice/prac-1/recap/app.js
+++ JavaScript-practice/prac-1/recap/app.js
@@ -7,3 +7,13 @@ num.forEach((number) => {
 //8 10 12 90 12
 
 document.write("<br>");
+
+//map creates a new array
+//it doesnt affect the orginal array, also printed the orginal array
+num.map((numm) => {
+  document.write(numm + 2 + " ");
+});
+// 6 7 8 47 8
+
+document.write("<br>" + num);
+// ,4,5,6,45,6
