56c017abe96af01c6a3a86b7373a9a7493001e33
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-20 01:17:30 +0530

    catch all segments review

--- tut20-NextJs4-Catch-all-Segments/myapp/src/app/cars/[...slug]/page.tsx
+++ tut20-NextJs4-Catch-all-Segments/myapp/src/app/cars/[...slug]/page.tsx
@@ -14,19 +14,35 @@ const page = ({
         <br />
         <h1>
           <a href="/cars/bmw">go back</a>
+          <a href="/cars/audi/rs7/2024">audi?</a>
         </h1>
       </div>
     );
   }
   if (parseInt(params.cars) > 1000) {
     notfound();
+  }
+
+  // so this would be after for 4
+  else if (params.slug.length === 3) {
+    return (
+      <div>
+        {params.slug[3]}
+        <h2>audi?</h2>
+        <a href="/cars/bmw"> go back?</a>
+      </div>
+    );
   } else if (params.slug.length === 1) {
     return (
       <div>
         {params.slug[0]}
         <h1>here are all the model we have</h1>
         <br />
-
+        <h1>
+          <a href="/cars/audi/rs7/2024">audi?</a>
+        </h1>
+        <br />
+        <br />
         <a href="cars/m3">m3</a>
         <br />
         <a href="cars/m4">m4</a>
