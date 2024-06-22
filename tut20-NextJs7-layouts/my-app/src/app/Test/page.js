db03d9a8d3f1efdc09d06dbe38441011d79af051
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-22 14:20:31 +0530

    Update to menu

--- tut20-NextJs7-layouts/my-app/src/app/Test/page.js
+++ tut20-NextJs7-layouts/my-app/src/app/Test/page.js
@@ -1,4 +1,3 @@
-// when using use state in next js make sure to import this
 "use client";
 import React, { useState } from "react";
 
@@ -10,28 +9,40 @@ const Page = () => {
   return (
     <div className="relative">
       <button
-        className="px-4 py-2 bg-blue-500 text-white rounded-md focus:outline-none"
+        className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         onClick={toggleDropdown}
       >
         Menu
       </button>
 
       {isOpen && (
-        <div className="absolute top-full left-0 w-40 bg-white shadow-md flex flex-col ">
-          <a href="#" className="px-4 py-2 gap-5 hover:bg-gray-100">
-            Test
+        <div className="absolute top-full left-0 w-40 bg-white shadow-lg flex flex-col mt-1 rounded-md">
+          <a
+            href="#"
+            className="block px-4 py-2 hover:bg-blue-100 text-blue-800"
+          >
+            Home <i className="fas fa-home"></i>
           </a>
-          <a href="#" className="px-4 py-2 gap-5 hover:bg-gray-100">
-            Test1
+          <a
+            href="#"
+            className="block px-4 py-2 hover:bg-green-100 text-green-800"
+          >
+            Profile <i className="fas fa-user"></i>
           </a>
-          <a href="#" className="px-4 py-2 gap-5 hover:bg-gray-100">
-            Test2
+          <a href="#" className="block px-4 py-2 hover:bg-red-100 text-red-800">
+            Messages <i className="fas fa-envelope"></i>
           </a>
-          <a href="#" className="px-4 py-2 mr-4 hover:bg-gray-100">
-            Test3
+          <a
+            href="#"
+            className="block px-4 py-2 hover:bg-yellow-100 text-yellow-800"
+          >
+            Settings <i className="fas fa-cog"></i>
           </a>
-          <a href="#" className="px-4 py-2 gap-5 hover:bg-gray-100">
-            Test4
+          <a
+            href="#"
+            className="block px-4 py-2 hover:bg-purple-100 text-purple-800"
+          >
+            Logout <i className="fas fa-sign-out-alt"></i>
           </a>
         </div>
       )}
