2d3dd15cb7621c33f7478d2315e648c5082e9dd3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-23 17:17:05 +0530

    extra meni

--- tut20-NextJs8-dropDownMenu/my-app/src/app/Menu/page.js
+++ tut20-NextJs8-dropDownMenu/my-app/src/app/Menu/page.js
@@ -3,7 +3,10 @@ import React, { useState } from "react";
 import Page from "../sai/Page";
 const page = () => {
   const [open, setOpen] = useState(false);
+  const [openn, setopenn] = useState(false);
+
   const toggleDropdown = () => setOpen(!open);
+  const toggleDropdownnn = () => setOpen(!open);
 
   return (
     <div>
@@ -16,6 +19,15 @@ const page = () => {
           <Page />
         </div>
       )}
+
+      <div>
+        <button onClick={toggleDropdownnn}> click me</button>
+        {openn && (
+          <div>
+            <a href="sai">sai</a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
