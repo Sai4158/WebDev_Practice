62b66e6cd3deee09fa46505f88cba02af7d60eb5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-23 23:50:25 +0530

    Routing Metadata in next js

---
+++ tut20-NextJs6-groupRoutes/my-app/src/app/(auth)/MetaData/page.js
@@ -0,0 +1,16 @@
+import { describe } from "node:test";
+import React from "react";
+
+//this for the meta data
+// improved seo
+export const metadata = {
+    //this will also change website title
+  title: "This is about the next",
+  description: "Genrated by next.js",
+};
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
