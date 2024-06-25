10907c549bfcae645c1f2211a72128434c377ec7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-25 16:27:06 +0530

    Set Attribute in js

--- JavaScript-practice/prac-15-attributes/Appa.js
+++ JavaScript-practice/prac-15-attributes/Appa.js
@@ -1,5 +1,11 @@
 // attributes in js
-//this will get the id name 
+//this will get the id name
 const att = document.querySelector("hello").attributes.id;
 
 document.write(att);
+
+//set attribute
+const attSet = document.querySelector("hello");
+// THIS IS WILL TAKE 2 PARAMETERS, one - what you want chnage
+// 2- the property of it
+attSet.setAttribute("style", "border: 5px solid black");
