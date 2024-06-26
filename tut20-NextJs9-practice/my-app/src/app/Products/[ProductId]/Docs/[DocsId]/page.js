1c74f921e897c12d53a5cfcde4800b5580273fcf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-26 23:55:23 +0530

    Dynamic routing in next js

--- tut20-NextJs9-practice/my-app/src/app/Products/[ProductId]/Docs/[DocsId]/page.js
+++ tut20-NextJs9-practice/my-app/src/app/Products/[ProductId]/Docs/[DocsId]/page.js
@@ -4,7 +4,7 @@ const page = ({ params }) => {
   return (
     <div>
       Product id <br />
-      <h1> Your search is {params.ProductId}</h1>
+      <h1> Your search is {params.DocsId}</h1>
     </div>
   );
 };
