32d743d3fe20757eebc1115a25c3dc20e2fe67e6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-27 22:22:38 +0530

    Link Component Navigation in next js

---
+++ tut20-NextJs10-Navigation/my-app/src/app/Homee/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+const page = () => {
+  return (
+    <div>
+      <div>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
