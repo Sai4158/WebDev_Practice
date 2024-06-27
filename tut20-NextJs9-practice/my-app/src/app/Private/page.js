265c55740dcebc9a596c6f902ca3d282e08fa939
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-27 17:35:54 +0530

    metadata in next js recap

---
+++ tut20-NextJs9-practice/my-app/src/app/Private/page.js
@@ -0,0 +1,13 @@
+//put this above the rafce then it will be used as the metadata
+export const metadata = {
+  title: "Hello i am sAI",
+  description: "Genrated by next js",
+};
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
