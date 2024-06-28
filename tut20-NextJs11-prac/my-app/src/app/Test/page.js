78ea5030c8409b45ce453df58fe4086a784f13e1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-29 01:30:05 +0530

    Next js with tailwiund css card

---
+++ tut20-NextJs11-prac/my-app/src/app/Test/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-50vh">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary color bg-green-600">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
