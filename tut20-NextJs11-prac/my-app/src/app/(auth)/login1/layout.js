aa8d72258fcef537b07fac0b9b0a6171d349a943
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-29 16:52:53 +0530

    Check if the link is active with ternary operator

--- tut20-NextJs11-prac/my-app/src/app/(auth)/login1/layout.js
+++ tut20-NextJs11-prac/my-app/src/app/(auth)/login1/layout.js
@@ -1,8 +1,9 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function AuthLayout({ children }) {
-  // put all the links here
   const navLinks = [
     { name: "Register", href: "/Register" },
     { name: "Login", href: "/Login" },
@@ -10,20 +11,26 @@ export default function AuthLayout({ children }) {
     { name: "Thar", href: "/thar" },
   ];
 
-  return (
-    // theen use a map function to print out the links
-    <div>
-      {navLinks.map((link) => (
-        <Link
-          className="text-purple-700 text-6xl underline"
-          href={link.href}
-          key={link.name}
-        >
-          {link.name}
-          <br /> <br />
-        </Link>
-      ))}
+  const pathname = usePathname();
 
+  return (
+    <div>
+      {navLinks.map((link) => {
+        const isActive = pathname === link.href;
+        return (
+          <Link
+            className={`text-purple-800 text-6xl ${
+              isActive ? "font-bold mr-4" : "text-blue-500 mr-4"
+            }`}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+            <br />
+            <br />
+          </Link>
+        );
+      })}
       {children}
     </div>
   );
