4aa215c844d11a6ccd990514d71405b721a864bc
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-04 00:35:24 +0530

    Will display data on the screen when submit type is clicked in js

--- JavaScript-practice/prac-18-FormListeners/app.js
+++ JavaScript-practice/prac-18-FormListeners/app.js
@@ -1,5 +1,15 @@
 // form events in js
 
-const mainform = document.getElementById("myForm");
+const mainform = document.getElementById("Myform");
 
 const username = document.getElementById("nameInput");
+
+const inputname11 = document.getElementById("name");
+
+mainform.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const myusername = username.value;
+
+  inputname11.textContent = myusername;
+});
