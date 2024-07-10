5e4758b975035c0677d42d47aaabd03a518b0fc1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-10 20:07:47 +0530

    added text on Experience page

--- A1_portfolio/my-app/src/app/(pages)/Experience/page.js
+++ A1_portfolio/my-app/src/app/(pages)/Experience/page.js
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
-};
+import Navbar from "@/app/Components/Navbar";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
 
-export default page;
+export default function Home() {
+  return (
+    <div>
+      <main className="pt-20 px-4">
+        <section id="experience" className="mt-20">
+          <h2 className="text-3xl font-semibold text-center">Experience</h2>
+          <div className="max-w-4xl mx-auto mt-10">
+            <div className="mb-8">
+              <h3 className="text-2xl font-semibold">Education</h3>
+              <p className="mt-2">
+                <strong>High School:</strong> Garnet Valley High School
+                <br />
+                <strong>University:</strong> Third year student at The
+                Pennsylvania State University, majoring in Human Centered Design
+                & Development in the College of Information Sciences &
+                Technology. Expected Graduation: May 2026
+              </p>
+            </div>
+            <div className="mb-8">
+              <h3 className="text-2xl font-semibold">Internship</h3>
+              <p className="mt-2">
+                <strong>Company:</strong> CTFGuide
+                <br />
+                <strong>Role:</strong> Software Engineer Intern
+                <br />
+                <strong>Duration:</strong> May 2024 - Present (3 months)
+                <br />
+                <strong>Location:</strong> State College, PA (Hybrid)
+                <br />
+                Responsible for building out user settings at CTFGuide. Used
+                React.js, TailwindCSS, Next.js, Git, and followed agile
+                development practices.
+              </p>
+            </div>
+            <div className="mb-8">
+              <h3 className="text-2xl font-semibold">Skills</h3>
+              <p className="mt-2">
+                <strong>Proficient in:</strong>
+                <br />
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
