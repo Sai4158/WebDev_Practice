d29545276f3f0ce27bbb9fde4bc6bc33f6b96735
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-14 20:06:51 +0530

    fixed homepage and vh

--- A1_portfolio/my-app/src/app/(pages)/(Home)/page.js
+++ A1_portfolio/my-app/src/app/(pages)/(Home)/page.js
@@ -4,9 +4,9 @@ import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 export default function Home() {
   return (
     <div>
-      <main className=" px-4">
-        <div className="flex  justify-center h-50">
-          <div className="relative w-full max-w-7xl bg-opacity-40 px-10 pb-10 rounded-lg shadow-lg overflow-hidden bg-black">
+      <main className="px-4">
+        <div className="flex justify-center min-h-screen">
+          <div className="relative w-full lg:h-[500px] max-w-7xl bg-opacity-40 px-6 pb-10 rounded-lg shadow-lg overflow-hidden bg-black">
             {/* Background Image */}
             <div className="absolute inset-0 hidden lg:block">
               <img
@@ -19,8 +19,8 @@ export default function Home() {
             {/* Content Section */}
             <div className="relative flex flex-col md:flex-row items-center justify-center w-full h-full">
               {/* Profile Picture Section */}
-              <div className="relative w-full md:w-1/3 flex justify-center md:justify-end p-8 md:p-16">
-                <div className="relative w-48 h-48 md:w-80 md:h-80 rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
+              <div className="relative w-full md:w-1/3 flex justify-center md:justify-end p-4 md:p-8 lg:p-16">
+                <div className="relative w-48 h-64 md:w-80 md:h-56 rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
                   <img
                     src="https://i.ibb.co/dQGwy7w/Sai-Profile-pic.jpg"
                     alt="Sai Rangineeni"
@@ -29,19 +29,19 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className="w-full md:w-2/3 mt-6 md:mt-0 md:ml-6 text-center md:text-left">
-                <h1 className="text-5xl font-bold text-white mb-4">
+              <div className="w-full md:w-2/3 mt-6 md:mt-0 md:ml-6 text-center md:text-left px-4 md:px-0">
+                <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
                   Sai Rangineeni
                 </h1>
-                <p className="text-xl text-gray-300 mb-4">
+                <p className="text-lg md:text-xl text-gray-300 mb-4">
                   PSU Student | Human-Centered Design & Development | Software
                   Engineer Intern @ CTFGuide
                 </p>
-                <p className="text-xl text-gray-300 mb-4">
+                <p className="text-lg md:text-xl text-gray-300 mb-4">
                   React.js, TailwindCSS, Next.js, JavaScript, Node.js, SQL,
                   MongoDB, Git
                 </p>
-                <p className="text-xl text-gray-300 mb-6">
+                <p className="text-lg md:text-xl text-gray-300 mb-6">
                   Interests: Web Development, UI/UX Design, Agile Development,
                   Music Production, Graphic Design
                 </p>
