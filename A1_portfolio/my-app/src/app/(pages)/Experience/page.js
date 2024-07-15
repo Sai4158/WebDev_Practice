1485cdd28e3114528cd30ac7c2c2a1ef6fab875f
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-15 20:16:24 +0530

    Added logo to the exp page

--- A1_portfolio/my-app/src/app/(pages)/Experience/page.js
+++ A1_portfolio/my-app/src/app/(pages)/Experience/page.js
@@ -1,10 +1,9 @@
 "use client";
 
-export default function page() {
+export default function Page() {
   return (
     <div>
       <main className="pt-1 pb-4 px-4">
-        {/* section 1  */}
         <section id="experience" className="">
           <h2 className="text-3xl font-semibold text-center">Experience</h2>
           <div className="max-w-4xl mx-auto mt-10">
@@ -14,38 +13,49 @@ export default function page() {
                 <strong>High School:</strong> Garnet Valley High School
                 <br />
                 <strong>University:</strong> Third year student at The
-                Pennsylvania State University, majoring in Human Centered Design
+                Pennsylvania State University, majoring in Human-Centered Design
                 & Development in the College of Information Sciences &
                 Technology. Expected Graduation: May 2026
               </p>
             </div>
 
-            {/* this is for the box */}
             <div className="bg-black bg-opacity-25 p-6 rounded-lg shadow-md mb-8">
               <h3 className="text-2xl font-semibold mb-2">Internship</h3>
-              <p>
-                <strong>Company:</strong> CTFGuide
-                <br />
-                <strong>Role:</strong> Software Engineer Intern
-                <br />
-                <strong>Duration:</strong> May 2024 - Present (3 months)
-                <br />
-                <strong>Location:</strong> State College, PA (Hybrid)
-                <br />
-                <br />
-                Responsible for building out user settings at CTFGuide. Used
-                React.js, TailwindCSS, Next.js, Git, and followed agile
-                development practices.
-              </p>
+              <div className="flex items-center">
+                <div className="flex-1">
+                  <p>
+                    <strong>Company:</strong> CTFGuide
+                    <br />
+                    <strong>Role:</strong> Software Engineer Intern
+                    <br />
+                    <strong>Duration:</strong> May 2024 - Present (3 months)
+                    <br />
+                    <strong>Location:</strong> State College, PA (Hybrid)
+                    <br />
+                    <br />
+                    As a Software Engineer Intern at CTFGuide, I contributed to
+                    building and optimizing user settings and profile settings.
+                    My role involved working extensively with Next.js,
+                    Javascript, TailwindCSS, React.js, Git, and agile
+                    development practices.
+                  </p>
+                </div>
+                {/* using div to make it next to each other using flex */}
+                <div className="flex-none ml-4">
+                  <img
+                    src="/Ctfguide.png"
+                    alt="CTFGuide Logo"
+                    width={200}
+                    height={200}
+                    className="hover:opacity-80 cursor-pointer"
+                    onClick={() => router.push("https://ctfguide.com")}
+                  />
+                </div>
+              </div>
             </div>
 
-            {/* section 3  */}
             <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-md">
               <h3 className="text-2xl font-semibold mb-4">Skills</h3>
-
-              {/* added Bubble for the skills  */}
-              {/* each span is Bubble */}
-
               <div className="flex flex-wrap gap-2 mt-6">
                 <span className="bg-blue-700 text-white py-2 px-4 rounded-full">
                   Java
