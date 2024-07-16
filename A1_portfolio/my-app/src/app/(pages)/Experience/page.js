94f4eeba4d243a45c4a086c45da18ab7fe8d6b21
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-16 17:09:45 +0530

    Changed span elements colors

--- A1_portfolio/my-app/src/app/(pages)/Experience/page.js
+++ A1_portfolio/my-app/src/app/(pages)/Experience/page.js
@@ -75,70 +75,70 @@ export default function Page() {
             <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-md">
               <h3 className="text-2xl font-semibold mb-4">Skills</h3>
               <div className="flex flex-wrap gap-2 mt-6">
-                <span className="bg-blue-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Java
                 </span>
-                <span className="bg-green-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   JavaScript
                 </span>
-                <span className="bg-purple-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   React.js
                 </span>
-                <span className="bg-pink-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Next.js
                 </span>
-                <span className="bg-red-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Express.js
                 </span>
-                <span className="bg-yellow-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Node.js
                 </span>
-                <span className="bg-indigo-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   SQL
                 </span>
-                <span className="bg-teal-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   MongoDB
                 </span>
-                <span className="bg-gray-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Tailwind CSS
                 </span>
-                <span className="bg-blue-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   HTML5
                 </span>
-                <span className="bg-orange-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Web Design
                 </span>
-                <span className="bg-red-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Web Development
                 </span>
-                <span className="bg-yellow-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Git
                 </span>
-                <span className="bg-green-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   CAD
                 </span>
-                <span className="bg-purple-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Audio Engineering
                 </span>
-                <span className="bg-pink-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   FL Studio
                 </span>
-                <span className="bg-indigo-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Adobe Audition
                 </span>
-                <span className="bg-teal-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Adobe Illustrator
                 </span>
-                <span className="bg-gray-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Video Editing
                 </span>
-                <span className="bg-blue-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   After Effects
                 </span>
-                <span className="bg-orange-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Adobe Photoshop
                 </span>
-                <span className="bg-yellow-700 text-white py-2 px-4 rounded-full">
+                <span className="bg-gray-800 text-white py-2 px-4 rounded-full">
                   Microsoft Office
                 </span>
               </div>
