9545527f632cac847760bd17e5ed5b424b1bb413
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-17 19:33:50 +0530

    Working animation

--- A1_portfolio/my-app/src/app/Components/PageTransition.js
+++ A1_portfolio/my-app/src/app/Components/PageTransition.js
@@ -16,12 +16,27 @@ const PageTransition = ({ children }) => {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }} // 1000ms transition duration
+        style={{ position: "relative" }}
       >
         {displayChildren}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 1 }}
+          transition={{ duration: 1 }} // Sync with the page transition
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+          }}
+        />
       </motion.div>
     </AnimatePresence>
   );
