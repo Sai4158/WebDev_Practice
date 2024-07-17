7e22ec8ce4c1e5464c0b51733f6d8d3302028332
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-17 19:13:07 +0530

    Added basic transition

--- A1_portfolio/my-app/src/app/Components/PageTransition.js
+++ A1_portfolio/my-app/src/app/Components/PageTransition.js
@@ -16,11 +16,10 @@ const PageTransition = ({ children }) => {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, filter: "blur(20px)" }}
-        animate={{ opacity: 1, filter: "blur(0px)" }}
-        exit={{ opacity: 0, filter: "blur(20px)" }}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 0.5 }}
-        style={{ position: "relative", minHeight: "100vh" }}
       >
         {displayChildren}
       </motion.div>
