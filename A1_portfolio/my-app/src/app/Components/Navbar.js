2535005b5cb5421171928d3e7320a5887a153d61
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-18 22:23:25 +0530

    added duration for more transitions

--- A1_portfolio/my-app/src/app/Components/Navbar.js
+++ A1_portfolio/my-app/src/app/Components/Navbar.js
@@ -20,8 +20,11 @@ const Navbar = () => {
   return (
     <header className="mt-5 pl-3 pr-5 left-0 w-full text-white z-50">
       <nav className="max-w-[85rem] w-full mx-auto px-4 flex items-center justify-between py-4">
-        <a className="text-2xl font-medium relative group" href="/">
-          <span className="absolute  opacity-0 group-hover:opacity-100 transition-opacity duration-600 text-blue-200">
+        <a
+          className="text-2xl font-medium relative group hover:scale-125 transition-transform duration-2000"
+          href="/"
+        >
+          <span className="absolute  opacity-0 group-hover:opacity-100 transition-opacity duration-600 text-blue-200 ">
             Home
           </span>
           <span className=" group-hover:opacity-0 transition-opacity duration-700   ">
@@ -39,28 +42,28 @@ const Navbar = () => {
         </div>
         <div className="hidden sm:flex space-x-8">
           <a
-            className="flex items-center font-medium hover:text-gray-300"
+            className="flex items-center font-medium hover:text-gray-300  hover:scale-95 transition-transform duration-2000"
             href="/"
           >
             <FaHome className="mr-2" />
             Home
           </a>
           <a
-            className="flex items-center font-medium hover:text-gray-300"
+            className="flex items-center font-medium hover:text-gray-300  hover:scale-95 transition-transform duration-2000"
             href="/Experience"
           >
             <FaUser className="mr-2" />
             Experience
           </a>
           <a
-            className="flex items-center font-medium hover:text-gray-300"
+            className="flex items-center font-medium hover:text-gray-300  hover:scale-95 transition-transform duration-2000"
             href="/Projects"
           >
             <FaProjectDiagram className="mr-2" />
             Projects
           </a>
           <a
-            className="flex items-center font-medium hover:text-gray-300"
+            className="flex items-center font-medium hover:text-gray-300  hover:scale-95 transition-transform duration-2000"
             href="/Contact"
           >
             <FaEnvelope className="mr-2" />
