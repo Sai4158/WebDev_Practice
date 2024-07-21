f42b650519a9ba204064674038644656ee551650
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-22 01:17:17 +0530

    relative group in tailwind css

--- tut20-NextJs11-prac/my-app/src/app/Test1/page.js
+++ tut20-NextJs11-prac/my-app/src/app/Test1/page.js
@@ -10,6 +10,16 @@ const page = () => {
         </span>
         <span className=" group-hover:opacity-0 ">Welcome!</span>
       </a>
+
+      <div>
+        <a href="" className="relative group">
+          {" "}
+          <span className=" absolute group-hover:opacity-100 opacity-0">
+            sai11111111
+          </span>
+          <span className=" group-hover:opacity-0">Sai</span>
+        </a>
+      </div>
     </div>
   );
 };
