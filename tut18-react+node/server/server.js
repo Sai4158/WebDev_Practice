20551b4ea050df827b48226cf46468e013b92e06
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-23 21:04:11 +0530

    Added backend data in the server.js file

--- tut18-react+node/server/server.js
+++ tut18-react+node/server/server.js
@@ -1,1 +1,13 @@
 // this where backend api will be
+
+const express = require("express");
+const app = express();
+
+app.get("/api", (res, req) => {
+  res.json({ users: ["Sai", "Abhi", "pranav"] });
+});
+
+// make sure this is runing in the diffrent port but not the same as the react
+app.listen(5000, () => {
+  console.log("Server stared on the port 5000");
+});
