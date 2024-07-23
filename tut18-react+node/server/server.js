3eedecf02070bff3e5521012d057d4b2a3962f28
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-23 22:10:39 +0530

    port 5000 for backend and 3000 for the frontend

--- tut18-react+node/server/server.js
+++ tut18-react+node/server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ users: ["Sai", "Abhi", "pranav"] });
+  res.json({ users: ["Sai", "Abhi", "pranav", "hello"] });
 });
 
 // make sure this is runing in the diffrent port but not the same as the react
