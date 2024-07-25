fc30adbedfb231d34d0a720635f4557fb3932ff1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-25 16:20:06 +0530

    Printing date and year using get method

--- JavaScript-practice/prac-19-date/app.js
+++ JavaScript-practice/prac-19-date/app.js
@@ -1,4 +1,17 @@
 const today = new Date();
 
-// this is will print the time 
+// this is will print the time
 document.write(today);
+
+document.write("<br>");
+document.write("<br>");
+
+//this is will print month
+// always add +1 since the index will start at the 0
+document.write(today.getMonth() + 1);
+
+document.write("<br>");
+document.write("<br>");
+
+// this will print the year
+document.write(today.getFullYear());
