37cf0492e3c57743c5ccd4b81b77cc3eb2f14cd2
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-27 02:34:05 +0530

    Math.random and methods

--- JavaScript-practice/prac-20-math/app.js
+++ JavaScript-practice/prac-20-math/app.js
@@ -1,12 +1,9 @@
 // this math in js
 
 // math.ceil method
-
-const ceil = Math.ceil(10.1);
-
-document.write(" <h1> ", ceil);
-
 // 11 - anything above the .1 it will assign to next number
+const ceil = Math.ceil(10.1);
+document.write(" <h1> ", ceil);
 
 document.write("<br>");
 // ------------------------
@@ -38,3 +35,15 @@ const min = Math.min(443, 34, 453, 76, 8435, 234);
 document.write(" <h1> ", min);
 
 document.write("<br>");
+
+// ------------------------
+
+// math random -  this will print  a random value when left a empty array
+const random = Math.random();
+document.write(" <h1> ", random);
+
+document.write("<br>");
+
+// this math random will print below 5
+const random1 = Math.random() * 5;
+document.write(" <h1> ", random1);
