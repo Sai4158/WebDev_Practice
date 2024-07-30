70c094964a953bd43014085345c529bc90032b68
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-07-30 15:56:08 +0530

    Using ...new Set to remove duplicates object

---
+++ JavaScript-practice/JS-tricky-1/app.js
@@ -0,0 +1,19 @@
+// this is how you can remvoe a duplicate obeckt in array
+
+const cars1 = ["Audi", "Benz", "BMW", "VW"];
+
+// add same "VW" from the array cars1
+const cars2 = ["tata", "maruti", "skoda", "VW"];
+
+// this is a function that will remove the duplicates
+// this will take cars1 and cars2
+
+function removeDUP(cars1, cars2) {
+  // merging both arrays using the spread operator
+  const merge = [...cars1, ...cars2];
+
+  //   using ... new Set will remove the duplicate objects in the array
+  const nodupe = [...new Set(merge)];
+}
+
+console.log(merge);
