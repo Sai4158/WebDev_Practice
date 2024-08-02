36639586fc488ced63a795cf83cc154b100dbff7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-02 09:16:51 +0530

    Added a button and tailwind css to use router

--- tut20-NextJs11-prac/my-app/src/app/Hello/[...Hello]/page.js
+++ tut20-NextJs11-prac/my-app/src/app/Hello/[...Hello]/page.js
@@ -6,13 +6,15 @@ const page = () => {
   const router = useRouter();
 
   return (
-    <div>
+    <div className=" justify-center place-items-center text-center bg-red-300 h-[100vh]">
+      <h1 className="pt-56 text-4xl"> This is Catch all segments page</h1>
       <button
+        className="mt-32  text-xl bg-slate-200 h-28 w-80 hover:scale-105 rounded-lg"
         onClick={() => {
           router.push("/");
         }}
       >
-        This is Catch all segments page
+        Click to go back to main page
       </button>
     </div>
   );
