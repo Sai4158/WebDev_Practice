3123888ca41d6965cc87b07c3fdd687940b829a9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-02 10:07:57 +0530

    Added a Image tag using next js

--- tut20-NextJs11-prac/my-app/src/app/page.js
+++ tut20-NextJs11-prac/my-app/src/app/page.js
@@ -3,14 +3,29 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 // this will be home page for this project
 
 const page = () => {
   const router = useRouter();
   return (
-    <div className=" justify-center place-items-center text-center bg-blue-300 h-[100vh]">
-      <h1 className="pt-56 text-4xl"> This is home page</h1>
+    <div className=" justify-center place-items-center text-center bg-blue-300 h-[150vh]">
+      {/* when you add a imgae address make sure to add domain in next config file too */}
+
+      <h1 className="pt-56 text-4xl">
+        {" "}
+        This is home page
+        <br />
+        <Image
+          src={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s"
+          }
+          width={400}
+          height={200}
+          className="text-center"
+        />
+      </h1>
 
       <button
         className="mt-24  text-xl bg-slate-200 h-28 w-80 hover:scale-105 rounded-lg"
