7f8cb16b8d14f81da4dd66cab6b0b250626d042c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-04 11:41:22 +0530

    API in next js working

--- tut21-NextJs11-UserDataFetching/my-app/src/app/api/cars/route.js
+++ tut21-NextJs11-UserDataFetching/my-app/src/app/api/cars/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ Student: "Hello World" });
+  return NextResponse.json({ student: "Hello World" });
 }
