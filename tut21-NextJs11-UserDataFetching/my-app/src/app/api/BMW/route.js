04eaf162a7b9f0e8c544509bb22a8db8afb604bd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-05 20:29:08 +0530

    NextResponse in next js using GET()

--- tut21-NextJs11-UserDataFetching/my-app/src/app/api/BMW/route.js
+++ tut21-NextJs11-UserDataFetching/my-app/src/app/api/BMW/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ student: "Hello World", teacher: "dwqs" });
+  return NextResponse.json({ BMW: "5 series" });
 }
