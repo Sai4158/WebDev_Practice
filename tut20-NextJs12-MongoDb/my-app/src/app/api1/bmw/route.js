48141d6f84b918a588eedd7ee4415cc2788484c7
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-06 14:42:08 +0530

    NextResponse and Mongoose in next js

--- tut20-NextJs12-MongoDb/my-app/src/app/api1/bmw/route.js
+++ tut20-NextJs12-MongoDb/my-app/src/app/api1/bmw/route.js
@@ -1,4 +1,10 @@
+import { DbCONNECT } from "@/app/utils/config/DB1";
 import { NextResponse } from "next/server";
+
+const connectDB1 = async () => {
+  await DbCONNECT();
+};
+connectDB1();
 
 export async function GET() {
   return NextResponse.json({ Student: "Sai", Car: "Range rover" });
---
+++ tut20-NextJs12-MongoDb/my-app/src/app/utils/config/DB1.js
@@ -0,0 +1,10 @@
+import mongoose, { Mongoose } from "mongoose";
+
+export const DbCONNECT = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB has been connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
