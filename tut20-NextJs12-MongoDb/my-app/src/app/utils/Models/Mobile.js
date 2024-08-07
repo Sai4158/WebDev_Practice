baef2690c8b5be0092c0f344e81f2d8d48d1b8fa
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-08 01:29:43 +0530

    Fix

--- tut20-NextJs12-MongoDb/my-app/src/app/utils/Models/Mobile.js
+++ tut20-NextJs12-MongoDb/my-app/src/app/utils/Models/Mobile.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const MobileSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true },
   model: { type: String, required: true, unique: true },
-  price: { type: Number, required: true }, 
+  price: { type: Number, required: true },
 });
-
 
 const MobileModel =
   mongoose.models.mobile || mongoose.model("mobile", MobileSchema);
