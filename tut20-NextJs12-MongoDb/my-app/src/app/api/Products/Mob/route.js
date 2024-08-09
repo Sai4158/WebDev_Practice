5fad5d8b892f72a44c71d9b2a7aabacd061bcd46
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-09 16:56:27 +0530

    Corrected the comments on the route js file

--- tut20-NextJs12-MongoDb/my-app/src/app/api/Products/Mob/route.js
+++ tut20-NextJs12-MongoDb/my-app/src/app/api/Products/Mob/route.js
@@ -3,21 +3,24 @@ import MobileModel from "@/app/utils/Models/Mobile";
 import { NextResponse } from "next/server";
 
 // connect to db
-// call the function here
-
 const connectDB = async () => {
   // call the connection function
   await DBconnection();
 };
+// call the function here to render
 connectDB();
 
+// this is to display data on the screen
 export async function GET() {
   const mobileData = await MobileModel.find({});
   return NextResponse.json(mobileData);
 }
 
+// this is to send data to DB
 export async function POST(request) {
   const { title, model, price } = await request.json();
   await MobileModel.create({ title, model, price });
   return NextResponse.json({ success: "Mobile added successfully!" });
 }
+
+// this function is t0 update the data
