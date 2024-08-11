55516129ff71399a593918c31bf6a15ba4ed2985
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-11 18:26:21 +0530

    Addec connection mongoDB

---
+++ tut20-NextJs13-MongoDb/my-app/src/app/Model/DB.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const Dbconnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Db connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- tut20-NextJs13-MongoDb/my-app/src/app/api/dogs/route.js
+++ tut20-NextJs13-MongoDb/my-app/src/app/api/dogs/route.js
@@ -1,4 +1,11 @@
+import { Dbconnection } from "@/app/Model/DB";
 import { NextResponse } from "next/server";
+
+// call the connection Db
+const connectDb = async () => {
+  await Dbconnection();
+};
+connectDb();
 
 export async function GET() {
   return NextResponse.json({ golden: "retriver" });
