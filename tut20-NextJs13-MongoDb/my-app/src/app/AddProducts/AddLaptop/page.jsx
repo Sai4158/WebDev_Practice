ae189156c24f1a52459c5b0b9be1e9eac4f54aea
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-13 16:35:01 +0530

    Posting data using a from to DB

---
+++ tut20-NextJs13-MongoDb/my-app/src/app/AddProducts/AddLaptop/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState } from "react";
+
+const page = () => {
+  const [name, Setname] = useState();
+  const [model, Setmodel] = useState();
+  const [price, Setprice] = useState();
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="space-y-4">
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Laptop name</h3>
+            <input
+              type="text"
+              value={name}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Laptop model</h3>
+            <input
+              type="text"
+              value={model}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Laptop price</h3>
+            <input
+              type="text"
+              value={price}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full mt-6 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-300"
+        >
+          Add Laptop
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
