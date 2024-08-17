e7115e216b96479e0affd79770404345e8eff0d1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-17 13:31:22 +0530

    making a form to get data by IDs

---
+++ tut20-NextJs12-MongoDb/my-app/src/app/All-products/Laptops/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>Laptop ID</th>
+          <th>Title</th>
+          <th>Model</th>
+          <th>Price</th>
+        </thead>
+      </table>
+    </div>
+  );
+};
+
+export default page;
