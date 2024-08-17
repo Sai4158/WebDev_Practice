af55d44a79f8dac1a237ce006c0429192d641d8e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-08-17 13:56:54 +0530

    Added form and printing data from DB

--- tut20-NextJs12-MongoDb/my-app/src/app/All-products/Laptops/page.jsx
+++ tut20-NextJs12-MongoDb/my-app/src/app/All-products/Laptops/page.jsx
@@ -1,17 +1,45 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const [laptops, SetLaptops] = useState([]);
+
+  const productHandle = async () => {
+    const response = await fetch("http://localhost:3000/api/Products/Laptops");
+    const resdata = await response.json();
+
+    console.log("checking for products", resdata);
+    SetLaptops(resdata.laptopsData);
+  };
+
+  useEffect(() => {
+    productHandle();
+  }, []);
+
   return (
     <div>
-      <table>
+      <h1>Laptop products </h1>
+      <table border={1}>
         <thead>
-          <th>Laptop ID</th>
-          <th>Title</th>
-          <th>Model</th>
-          <th>Price</th>
+          {/* table row */}
+          <tr>
+            {/* table heading */}
+            <th>Laptop ID</th>
+            <th>Title</th>
+            <th>Model</th>
+            <th>Price</th>
+          </tr>
         </thead>
+        <tbody>
+          {laptops.map((item) => {
+            return (
+              <tr>
+                <td key={item._id}>{item._id}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
