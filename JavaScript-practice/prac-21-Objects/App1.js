1f1b086b7f7a02fc14ad7a761952bbc4b8e0eb76
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-03 15:41:04 -0400

    Objects in Js

---
+++ JavaScript-practice/prac-21-Objects/App1.js
@@ -0,0 +1,2 @@
+const aaa = ["43", "34", "324"];
+console.log(aaa[1]);
---
+++ JavaScript-practice/prac-21-Objects/app.js
@@ -0,0 +1,13 @@
+const aaa = ["43", "34", "324"];
+// printing the Index 1 in the array
+// for the array u fetch using the Index
+console.log(aaa[1]);
+
+// making a object
+// for ovjects you fetch using the KEY
+const captial = {
+  India: "Contry",
+  Hyderbad: "State",
+};
+
+console.log(captial.India);
