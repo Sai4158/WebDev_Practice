a1eb2dbcd0198989d43ed7642431f8abbaba1f38
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-06 10:47:30 -0400

    Added a get req

--- tut21-NextJs11-UserDataFetching/my-app/src/app/api/BMW/route.js
+++ tut21-NextJs11-UserDataFetching/my-app/src/app/api/BMW/route.js
@@ -3,3 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ BMW: "5 series" });
 }
+
+export async function GET() {
+  return NextResponse.json({ car: "BMW" });
+}
