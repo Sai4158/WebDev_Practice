d3a382edb0a34fb19395aa73b7361fc90684c6e8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-13 00:04:19 -0400

    added GET method

---
+++ tut21-NextJs12-Recap/my-app/src/app/API/Monitors/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+const { DBconnection } = require("@/app/utils/config/db");
+
+const connectDB = async () => {
+  await DBconnection();
+};
+
+// get function
+
+export async function GET() {
+  connectDB();
+
+  return NextResponse.json({ msg: "Succefully done" });
+}
