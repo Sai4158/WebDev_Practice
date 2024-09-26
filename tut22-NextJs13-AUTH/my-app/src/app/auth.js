d28a718a5cc41a445b3e500e8786246703ab4766
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-26 01:28:31 -0400

    This is updated code

--- tut22-NextJs13-AUTH/my-app/src/app/auth.js
+++ tut22-NextJs13-AUTH/my-app/src/app/auth.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
+// updated code
 export const authOptions = {
   providers: [
     GithubProvider({
