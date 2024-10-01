b18e0dc8815a61ac5a4d392340a36ea3a80e21bf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-01 18:55:04 -0400

    Added a form and CSS

---
+++ tut23-NextJs15-AUTH-Credentials/my-app/src/app/register/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+// This if for the login form
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="form-container">
+      <form action="" className="formSection">
+        <h3>Username:</h3>
+        <input type="text" name="" id="" className="input1" />
+
+        <h3>Email:</h3>
+        <input type="email" name="" id="" className="input1" />
+
+        <h3>Password:</h3>
+        <input type="password" name="" id="" className="input1" />
+
+        <br />
+        <br />
+        <button type="submit" className="button">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
