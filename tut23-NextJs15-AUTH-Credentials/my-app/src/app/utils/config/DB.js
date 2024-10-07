0750a708f0e863936ea7861dff0757e7b12f8790
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-07 19:59:12 -0400

    Added registerAction

--- tut23-NextJs15-AUTH-Credentials/my-app/src/app/utils/config/DB.js
+++ tut23-NextJs15-AUTH-Credentials/my-app/src/app/utils/config/DB.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CON);
+    await mongoose.connect(process.env.MONGODB);
     console.log("Db has been connected succefully");
   } catch (error) {
     console.log(error);
