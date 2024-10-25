69c92aa1004b894a208a91308b40f3f0d741ad57
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-25 18:38:27 -0400

    Takes input from form and submit them

--- tut20-NextJs15-AUTH-Credentials/my-app/src/app/Register/Page.js
+++ tut20-NextJs15-AUTH-Credentials/my-app/src/app/Register/Page.js
@@ -1,26 +1,65 @@
-"Use Client";
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  // Set state for the form fields
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const registerDetails = { username, email, password };
+    console.log("Register Details:", registerDetails);
+
+    // Reset them after
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <form className="bg-white p-6 rounded-md shadow-md w-96">
-        <h3 className="text-lg mb-4">Register</h3>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded-md shadow-md w-96"
+      >
+        <h3 className="text-lg mb-4 text-center">Register Form</h3>
 
         <div className="mb-4">
           <label className="block mb-1">Username</label>
-          <input type="text" className="border p-2 rounded w-full" />
+          <input
+            type="text"
+            className="border p-2 rounded w-full"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="username"
+          />
         </div>
 
         <div className="mb-4">
           <label className="block mb-1">Email</label>
-          <input type="email" className="border p-2 rounded w-full" />
+          <input
+            type="email"
+            className="border p-2 rounded w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+          />
         </div>
 
         <div className="mb-4">
           <label className="block mb-1">Password</label>
-          <input type="password" className="border p-2 rounded w-full" />
+          <input
+            type="text"
+            className="border p-2 rounded w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+          />
         </div>
 
         <button
