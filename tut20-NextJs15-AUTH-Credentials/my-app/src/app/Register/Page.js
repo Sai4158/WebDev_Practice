040bae075bb164190398c02227c2e80168c8cf34
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-25 17:44:10 -0400

    Added tailwind css

--- tut20-NextJs15-AUTH-Credentials/my-app/src/app/Register/Page.js
+++ tut20-NextJs15-AUTH-Credentials/my-app/src/app/Register/Page.js
@@ -4,16 +4,60 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <form action="">
-        <h3>Username </h3>
-        <input type="text" />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 max-w-lg w-full">
+        <h3 className="text-xl font-semibold mb-4 text-center">Register</h3>
 
-        <h3>Email </h3>
-        <input type="text" />
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
 
-        <h3>Password </h3>
-        <input type="text" />
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            type="text"
+            id="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Register
+          </button>
+        </div>
       </form>
     </div>
   );
