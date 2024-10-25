5b4715a09c11851c385a2cb11ab665e5937cbcfd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-25 17:39:36 -0400

    Added sign in form

---
+++ tut20-NextJs15-AUTH-Credentials/my-app/src/app/Register/Page.js
@@ -0,0 +1,22 @@
+"Use Client";
+
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <form action="">
+        <h3>Username </h3>
+        <input type="text" />
+
+        <h3>Email </h3>
+        <input type="text" />
+
+        <h3>Password </h3>
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
