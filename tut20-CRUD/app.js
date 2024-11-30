8217afe0b5fbddc969a2a17e40ca608b4c6893eb
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-30 16:04:21 -0500

    Added post method

--- tut20-CRUD/app.js
+++ tut20-CRUD/app.js
@@ -1,12 +1,16 @@
+const { connect } = require("mongoose");
+const { AzureOpenAI } = require("openai");
+
 const express = require(express);
 const { MongoClient } = require(MongoClient);
 
 const app = express();
 const port = 3000;
+let db;
 
 // MongoDb connection
-const uri = "";
-const dbname = " ";
+const uri =
+  "mongodb+srv://learn123:learn123@cluster0.ern8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 async function ConnectedtoDB() {
   try {
@@ -17,4 +21,19 @@ async function ConnectedtoDB() {
   }
 }
 
-ConnectedtoDB();
+app.post("/users", async (req, res) => {
+  try {
+    const user = req.body;
+    const result = await Db.collection("users").insertone(user);
+    res.send({ message: "User added", userId: result.insertedId });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Start the server
+
+app.listen(port, async () => {
+  await ConnectedtoDB();
+  console.log("Db has been connected");
+});
