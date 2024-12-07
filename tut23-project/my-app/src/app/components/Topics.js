bd341fb27ff513061ef4f93ed5216acea8d58e77
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-06 22:45:48 -0500

    Added many topics for testing (working)

--- tut23-project/my-app/src/app/components/Topics.js
+++ tut23-project/my-app/src/app/components/Topics.js
@@ -5,12 +5,13 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const Topics = () => {
   return (
-    <div>
+    <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start mx-28">
       <div>
-        <h2>Topic name</h2>
+        <h2 className="font-bold-text text-2xl">Topic name</h2>
         <div>Topic description</div>
       </div>
-      <div>
+
+      <div className="flex gap-2">
         <RemoveBtn />
         <HiPencilAlt size={24} />
         <Link href={"/EditTopic/123"}></Link>
