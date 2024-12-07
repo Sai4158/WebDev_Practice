c9cbdfd12f9360cef44d543055bf5a576786d99d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-06 22:49:41 -0500

    Added AddTopic page

---
+++ tut23-project/my-app/src/app/AddTopic/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return <div>
+
+    <form action="">
+
+        <input type="text" placeholder="Topic" />
+    </form>
+  </div>;
+};
+
+export default page;
