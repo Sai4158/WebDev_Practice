8b7425004408a206190e41838ccb0ac245507488
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-16 20:33:05 -0500

    Added Getopics function to get topics from the DB to display

--- tut23-project/my-app/src/app/components/Topics.js
+++ tut23-project/my-app/src/app/components/Topics.js
@@ -3,7 +3,27 @@ import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-const Topics = () => {
+// Gets topic information form the DB
+
+const getTopic = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/models/Api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Topics = async () => {
+  await getTopic();
+
+  const topic = await getTopic();
   return (
     <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start mx-28">
       <div>
