9aef4dca73696c42f39fc5b2c71fd76d3acc09cf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-17 17:10:24 -0500

    Adding a post method to update data from the input to the DB

--- tut23-project/my-app/src/app/AddTopic/page.js
+++ tut23-project/my-app/src/app/AddTopic/page.js
@@ -1,10 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const page = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventdefault();
+
+    if (!title || !description) {
+      alert("Please enter data");
+      return;
+    }
+
+    try {
+      await fetch("http://localhost:3000/models/Api/topics", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, description }),
+      });
+    } catch (error) {}
+  };
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-3 mx-80">
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-3 mx-80"
+      >
         <input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
           type="text"
           placeholder="Topic Title"
           className="border border-slate-500 px-8 py-2"
@@ -12,6 +40,8 @@ const page = () => {
 
         {/* Topic desc */}
         <input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
           type="text"
           placeholder="Topic description"
           className="border border-slate-500 px-8 py-2"
