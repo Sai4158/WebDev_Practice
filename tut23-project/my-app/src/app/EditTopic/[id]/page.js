b9879c8f4a0141742b387c85c44858b2a18461b8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-12-18 16:49:36 -0500

    Adding edit button to update the forms,

--- tut23-project/my-app/src/app/EditTopic/[id]/page.js
+++ tut23-project/my-app/src/app/EditTopic/[id]/page.js
@@ -1,10 +1,35 @@
 import EditTopicForm from "@/app/components/EditTopicForm";
 import React from "react";
 
-const page = () => {
+const page = async ({ params }) => {
+  const { id } = params;
+
+  const getTopicById = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/models/Api/topics/${id}`, {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch the topic");
+      }
+      return res.json();
+    } catch (error) {
+      console.log("Error fetching topic:", error);
+      return null;
+    }
+  };
+
+  const topic = await getTopicById();
+
+  if (!topic) {
+    return <div>Error: Topic not found</div>;
+  }
+
+  const { title, description } = topic;
+
   return (
     <div>
-      <EditTopicForm />
+      <EditTopicForm id={id} title={title} description={description} />
     </div>
   );
 };
