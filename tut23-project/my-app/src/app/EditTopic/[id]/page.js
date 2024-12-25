05a5296474ad540f4cf1e997a6a84120bac5c298
Sai Rangineeni <116776995+Sai4158@users.noreply.github.com>
2024-12-25 12:22:49 -0500

    Update page.js

--- tut23-project/my-app/src/app/EditTopic/[id]/page.js
+++ tut23-project/my-app/src/app/EditTopic/[id]/page.js
@@ -30,7 +30,7 @@ const page = async ({ params }) => {
 
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold mb-6 font-extralight">Edit Topic</h1>
+      <h1 className="text-3xl font-bold mb-6 font-extralight">change topic</h1>
       <EditTopicForm id={id} title={title} description={description} />
     </div>
   );
