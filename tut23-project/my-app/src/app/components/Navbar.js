d841bf7c0adcb1cec6399bdd835478732a7ccb23
Sai Rangineeni <116776995+Sai4158@users.noreply.github.com>
2024-12-30 01:15:37 -0500

    Update Navbar.js

--- tut23-project/my-app/src/app/components/Navbar.js
+++ tut23-project/my-app/src/app/components/Navbar.js
@@ -6,9 +6,12 @@ export default function NavBar() {
       <Link className="text-white font-bold " href={"/"}>
         Home
       </Link>
-      <Link className="bg-white p-2 rounded-lg " href={"/AddTopic"}>
-        Add Topic
-      </Link>
+<Link
+  className="bg-white p-2 rounded-lg shadow-md hover:bg-gray-100 hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+  href="/AddTopic"
+>
+  Add Topic
+</Link>
     </nav>
   );
 }
