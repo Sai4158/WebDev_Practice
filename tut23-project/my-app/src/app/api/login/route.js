bb8e4ebb86b8216ce3b28634afb795052eb683a0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-26 17:53:40 -0400

    Added working login with user check and password validation

---
+++ tut23-project/my-app/src/app/api/login/route.js
@@ -0,0 +1,29 @@
+import User from "@/app/Models/User";
+import connectToDb from "@/app/lib/db";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    await connectToDb();
+    const { email, password } = await request.json();
+    const userExsit = await User.findOne({ email });
+
+    if (!userExsit) {
+      return NextResponse.json({ error: "User does not exist" });
+    }
+
+    const CheckPassword = await bcrypt.compare(password, userExsit.password);
+
+    if (!CheckPassword) {
+      return NextResponse.json({
+        error: "Password does not match",
+        status: 404,
+      });
+    }
+
+    return NextResponse.json({ message: "Login is successfully", status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Error has oocured", status: 500 });
+  }
+}
--- tut23-project/my-app/src/app/login/page.js
+++ tut23-project/my-app/src/app/login/page.js
@@ -1,11 +1,59 @@
-import React from "react";
+"use client";
 
-const LoginPage = () => {
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const router = useRouter();
+
+  const onHandle = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/login", {
+      email,
+      password,
+    });
+
+    console.log(response);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center text-3xl">
-      LoginPage
+    <div className="min-h-screen flex items-center justify-center bg-gray-300">
+      <form
+        onSubmit={onHandle}
+        className="bg-white p-8 rounded-xl shadow-md w-96 space-y-4"
+      >
+        <h2 className="text-2xl font-bold text-center">Login</h2>
+
+        <div className="flex flex-col">
+          <label className="mb-1">Email</label>
+          <input
+            type="email"
+            onChange={(e) => setemail(e.target.value)}
+            placeholder="Enter Email"
+            className="px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="mb-1">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setpassword(e.target.value)}
+            placeholder="*********"
+            className="px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+        >
+          Login
+        </button>
+      </form>
     </div>
   );
-};
-
-export default LoginPage;
+}
